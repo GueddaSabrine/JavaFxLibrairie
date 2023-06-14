da26364de125175c2d2cb3e46038c1212dc48f68
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2023-06-14 21:16:34 +0200

    configuration du fichier pom.xml

--- src/test/java/org/openjfx/javafxmavenarchetypesTests/Controller/FormControllerTest.java
+++ src/test/java/org/openjfx/javafxmavenarchetypesTests/Controller/FormControllerTest.java
@@ -2,7 +2,7 @@ package org.openjfx.javafxmavenarchetypesTests.Controller;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
 public class FormControllerTest {
 
 }
