dd39538b99c120acad1310eb4a67d3ddcf7f199a
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2023-06-15 10:48:45 +0200

    test pom

--- src/test/java/org/openjfx/javafxmavenarchetypesTests/Controller/FormControllerTest.java
+++
@@ -1,8 +0,0 @@
-package org.openjfx.javafxmavenarchetypesTests.Controller;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-
-//@ExtendWith(MockitoExtension.class)
-public class FormControllerTest {
-
-}
