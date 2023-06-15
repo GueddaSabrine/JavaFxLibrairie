dbe8b4e6e80f0a74eebc23c16cef6399a7c70e4f
alex78590 <alexisdrouhincohen@gmail.com>
2023-06-15 12:21:42 +0200

    test

--- src/main/java/org/openjfx/javafxmavenarchetypes/controller/FormController.java
+++ src/main/java/org/openjfx/javafxmavenarchetypes/controller/FormController.java
@@ -27,10 +27,6 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
-
-import javax.xml.bind.Unmarshaller;
-
-import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,6 +36,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 
 public class FormController  {
@@ -76,6 +73,7 @@ public class FormController  {
     @FXML
     public TableColumn <Bibliotheque.Livre, String> colTitre;
 
+
     //Tableview
     @FXML
     public TableView <Bibliotheque.Livre> tableau;
@@ -100,12 +98,15 @@ public class FormController  {
     //mmh ...
     Bibliotheque.Livre selectedbook = null ;
     File selectedFile = null;
+
+    boolean fileSaved ;
     @FXML
     public void initialize(){
 
         inittableau();
         btnMoins.setDisable(true);
         setDefaultTextField();
+        fileSaved = false;
     }
 
     public void inittableau(){
@@ -209,10 +210,13 @@ public class FormController  {
         tableau.refresh();
 
 
-    }
 
+    }
     @FXML
     public void handleSaveAs(ActionEvent event) throws JAXBException {
+
+
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Enregistrer");
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Fichier XML", "*.xml"));
@@ -223,7 +227,7 @@ public class FormController  {
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             System.out.println("ok");
             jaxbMarshaller.marshal(bibliotheque, selectedFile);
-
+            fileSaved = true;
 
         }
 
@@ -237,7 +241,7 @@ public class FormController  {
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             System.out.println("ok");
             jaxbMarshaller.marshal(bibliotheque, selectedFile);
-
+            fileSaved = true;
 
         }
         else{
@@ -270,7 +274,7 @@ public class FormController  {
 
             ObservableList<Bibliotheque.Livre> listD = getListData();
             tableau.setItems(listD);
-
+            fileSaved = true;
 
         }
 
@@ -313,36 +317,47 @@ public class FormController  {
             ObservableList<Bibliotheque.Livre> listD = getListData();
             tableau.setItems(listD);
             tableau.refresh();
-        }
-    }
-
-    public void handleOpen(ActionEvent event) throws JAXBException, SAXException {
-        File xsdf = new File("src/main/xsd/Biblio.xsd");
-
-        /* ouverture du fichier xml */
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Ouvrir");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Fichier XML", "*.xml"));
-        selectedFile = fileChooser.showOpenDialog(tableau.getScene().getWindow());
-        if (selectedFile != null){
-            //unmarshalling ( xml -> java)
-            JAXBContext jaxbContext = JAXBContext.newInstance(Bibliotheque.class);
-            Unmarshaller jaxbunMarshaller = jaxbContext.createUnmarshaller();
-            SchemaFactory schemafactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-
-            //try
-            Schema sch  = schemafactory.newSchema(xsdf);
-            jaxbunMarshaller.setSchema(sch);
-            bibliotheque= (Bibliotheque) jaxbunMarshaller.unmarshal(selectedFile);
-            bibliotheque.print();
-
-            /* mise a jour du tableau d'affichage */
-
-            ObservableList<Bibliotheque.Livre> listD = getListData();
-            tableau.setItems(listD);
-
-
+            fileSaved = false;
         }
 
     }
+
+    public void handleExit(){
+
+        if(!fileSaved){
+
+
+        }
+    }
+
+    public boolean AlerteSAuvegarde(){
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("Look, a Confirmation Dialog");
+        alert.setContentText("Are you ok with this?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK
+        } else {
+            // ... user chose CANCEL or closed the dialog
+        }
+        return true;
+    }
+
+    public void AlerteAddModifyBookDone(){
+
+    }
+
+    public boolean AlerteModifyBook(){
+        return true;
+
+    }
+
+    public boolean AlerteSuppBook(){
+        return true;
+
+    }
+
 }
