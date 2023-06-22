09f9a01287a2ef49f8c6074272c19bea3f55e94e
alex78590 <alexisdrouhincohen@gmail.com>
2023-06-22 12:12:28 +0200

    before pull (XML handler debut)

--- src/main/java/org/openjfx/javafxmavenarchetypes/controller/FormController.java
+++ src/main/java/org/openjfx/javafxmavenarchetypes/controller/FormController.java
@@ -18,6 +18,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 //import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 import org.openjfx.javafxmavenarchetypes.model.Bibliotheque;
+import org.openjfx.javafxmavenarchetypes.model.XMLhandler;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -96,6 +97,7 @@ public class FormController  {
     //Bibliotheque
 
     public Bibliotheque bibliotheque = new Bibliotheque();
+    public XMLhandler xmlfile = new XMLhandler();
 
     //Bouton
 
@@ -351,20 +353,7 @@ public class FormController  {
     public void handleSaveAs(ActionEvent event) throws JAXBException {
 
 
-
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Enregistrer");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Fichier XML", "*.xml"));
-        selectedFile = fileChooser.showSaveDialog(tableau.getScene().getWindow());
-        if (selectedFile != null){
-            JAXBContext jaxbContext = JAXBContext.newInstance(Bibliotheque.class);
-            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            System.out.println("ok");
-            jaxbMarshaller.marshal(bibliotheque, selectedFile);
-            fileSaved = true;
-
-        }
+        xmlfile.SaveAs(tableau.getScene().getWindow(), bibliotheque);
 
     }
 
